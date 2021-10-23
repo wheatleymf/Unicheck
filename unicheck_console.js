a8d8f07f26cd88335b0a867a9e4c5a00854783e2
WheatleyMF <wheatleyinthespace@yandex.ru>
2021-10-24 00:43:29 +0300

    Single character check improvements

--- unicheck_console.js
+++ unicheck_console.js
@@ -7,12 +7,17 @@ let Unicheck = {
         return [text, length];
     },
     SingleChar: function(id, pos) {
+        if (pos == undefined) {
+            console.error("Position must be specified when calling a single character check.");
+            return false;
+        }
         let content = Unicheck.Get(id);
 
-        let simple = content[0].codePointAt(pos);
-        let hex = content[0].codePointAt(pos).toString(16);
+        let simple = content[0].codePointAt(i);
+        let reverse = String.fromCodePoint(simple);
+        let hex = content[0].codePointAt(i).toString(16);
         let output = "\\u" + "0000".substring(0, 4 - hex.length) + hex;
-        console.log(`[ID <i>${id}</i> | POSITION <i>${pos}</i>] Character code for position <i>${pos}</i> is <i>${output}</i> (simple: <i>${simple}</i>)`);
+        console.log(`>> POSITION ${pos} SYMBOL "${reverse}" || CHARACTER CODE IS ${output} (simple: ${simple})`);
     },
     AllChars: function(id) {
         let content = Unicheck.Get(id);
