2efcfd53ffdd9770fb32b30b0bdea3a7fa45c7b7
WheatleyMF <wheatleyinthespace@yandex.ru>
2021-10-24 13:02:48 +0300

    Some more simplification

--- unicheck_console.js
+++ unicheck_console.js
@@ -1,33 +1,34 @@
 let Unicheck = {
-    Get: function(id) {
+    Get: function (id) {
         let text = document.getElementById(id).innerText;
         let length = text.length;
         console.log(`Copied message is "${text}", length is ${length}`);
 
         return [text, length];
     },
-    SingleChar: function(id, pos) {
+    Read: function (content, pos) {
+        let simple = content.codePointAt(pos);
+        let reverse = String.fromCodePoint(simple);
+        let hex = content.codePointAt(pos).toString(16);
+        let output = "\\u" + "0000".substring(0, 4 - hex.length) + hex;
+
+        return [simple, reverse, output];
+    },
+    SingleChar: function (id, pos) {
         if (pos == undefined) {
             console.error("Position must be specified when calling a single character check.");
             return false;
         }
         let content = Unicheck.Get(id);
-
-        let simple = content[0].codePointAt(i);
-        let reverse = String.fromCodePoint(simple);
-        let hex = content[0].codePointAt(i).toString(16);
-        let output = "\\u" + "0000".substring(0, 4 - hex.length) + hex;
-        console.log(`>> POSITION ${pos} SYMBOL "${reverse}" || CHARACTER CODE IS ${output} (simple: ${simple})`);
+        let result = Unicheck.Read(content[0], pos);
+        console.log(`>> POSITION ${pos} SYMBOL "${result[1]}" || CHARACTER CODE IS ${result[2]} (simple: ${result[0]})`);
     },
-    AllChars: function(id) {
+    AllChars: function (id) {
         let content = Unicheck.Get(id);
 
         for (let i = 0; i < content[1]; i++) {
-            let simple = content[0].codePointAt(i);
-            let reverse = String.fromCodePoint(simple);
-            let hex = content[0].codePointAt(i).toString(16);
-            let output = "\\u" + "0000".substring(0, 4 - hex.length) + hex;
-            console.log(`>> POSITION ${i} SYMBOL "${reverse}" || CHARACTER CODE IS ${output} (simple: ${simple})`);
+            let result = Unicheck.Read(content[0], i);
+            console.log(`>> POSITION ${i} SYMBOL "${result[1]}" || CHARACTER CODE IS ${result[2]} (simple: ${result[0]})`);
         }
     }
 }
